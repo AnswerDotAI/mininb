4bf4f77178446dd4a5e7b3e67679912ea1f092cc
Jeremy Howard <github@jhoward.fastmail.fm>
2024-05-16 14:44:23 +1000

    delete and view working; no litestar

--- app.py
+++ app.py
@@ -1,91 +1,114 @@
-import types, xml.etree.ElementTree as ET
+import uvicorn, socket, time
+
 from dataclasses import dataclass
-from functools import partial
-from litestar import MediaType, Litestar, get, post, put
-from litestar.static_files import create_static_files_router
-from litestar.exceptions import NotFoundException
+from httpx import get,post
+from IPython import display
+from functools import wraps
 
-def _attrmap(o):
-    o = o.lstrip('_').replace('_','-')
-    return dict(cls='class', klass='class', fr='for').get(o, o)
+from fastcore.utils import *
+from fastcore.xml import *
 
-def xt(tag:str, *c, **kw):
-    if len(c)==1 and isinstance(c[0], types.GeneratorType): c = tuple(c[0])
-    elif len(c)==1 and isinstance(c[0],str): c = c[0]
-    kw = {_attrmap(k):str(v) for k,v in kw.items()}
-    return [tag.lower(),c,kw]
-
-g = globals()
-tags = '''html head title meta link style body div span p h1 h2 h3 h4 h5 h6 strong em b i u s
-strike sub sup hr br img a link nav ul ol li dl dt dd table thead tbody tfoot tr th td caption
-col colgroup form input textarea button select option label fieldset legend details summary main
-header footer section article aside figure figcaption mark small iframe object embed param video
-audio source canvas svg math script noscript template slot'''.split()
-for o in tags: g[o.capitalize()] = partial(xt, o)
-
-def to_xml(node:tuple):
-    "Convert `node` to an XML string."
-    def mk_el(tag, cs, attrs):
-        el = ET.Element(tag, attrib=attrs)
-        if isinstance(cs, tuple): el.extend([mk_el(*o) for o in cs])
-        elif cs is not None: el.text = str(cs)
-        return el
-
-    root = mk_el(*node)
-    ET.indent(root)
-    return ET.tostring(root, encoding='unicode', short_empty_elements=False)
+from starlette.applications import Starlette
+from starlette.routing import Route, Mount, WebSocketRoute
+from starlette.responses import Response, HTMLResponse, RedirectResponse, FileResponse
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException
 
 @dataclass
 class TodoItem:
-    id: int
-    title: str
-    done: bool
-    def tag(self): return A(self.title, hx_get=f'/todos/{self.id}', hx_target="#current-todo", href='#')
+    id: int; title: str; done: bool
+    def __xt__(self):
+        before = "console.log('Making a request!')"
+        after = "console.log('Done making a request!')"
+        link = A(self.title, href='#',
+                hx_get=f'/todos/{self.id}', hx_target="#current-todo",
+                hx_on__after_request=after, hx_on__before_request=before)
+        return Li(link, id=f'todo-{self.id}')
 
 TODO_LIST: list[TodoItem] = [
-    TodoItem(0, title="Start writing TODO list", done=True),
-    TodoItem(1, title="???", done=False),
-    TodoItem(2, title="Profit", done=False),
+    TodoItem("0", title="Start writing TODO list", done=True),
+    TodoItem("1", title="???", done=False),
+    TodoItem("2", title="Profit", done=False),
 ]
 
-def get_todo_by_title(todo_name) -> str:
-    try: return next(o for o in TODO_LIST if o.title == todo_name).title
-    except: raise NotFoundException(detail=f"TODO {todo_name!r} not found") from None
+def wrap_root(resp, headtags):
+    title = Title('Page')
+    if isinstance(resp, tuple): title,resp = resp
+    return Html(Head(title, *headtags), resp)
 
-@get("/todos/{todoid:int}", media_type=MediaType.HTML)
-async def get_todo_by_id(todoid:int) -> str:
-    try: todo = next(o for o in TODO_LIST if o.id == todoid)
-    except: raise NotFoundException(detail=f"TODO {todoid!r} not found") from None
-    return to_xml(Div(Div(todo.title), A('delete', hx_delete=f'/todos/{todo.id}', href='#')))
+class Router:
+    def __init__(self, headtags=None, routes=None):
+        self.rt = routes or []
+        self.rd = {}
+        self.headtags = headtags or []
 
-@post("/")
-async def add_item(data: TodoItem) -> list[TodoItem]:
+    def __call__(self, path, meth='GET', cls=HTMLResponse):
+        def _inner(f):
+            @wraps(f)
+            async def g(request):
+                resp = await f(request)
+                if not resp or isinstance(resp, str): resp = cls(resp)
+                if isinstance(resp, Response): return resp
+                if 'HX-Request' not in request.headers:
+                    resp = wrap_root(resp, self.headtags)
+                return cls(to_xml(resp))
+            self.rd[(path,meth)] = Route(path, g, methods=[meth])
+            return g
+        return _inner
+
+    @property
+    def routes(self): return self.rt + list(self.rd.values())
+
+class NotFoundException(HTTPException):
+    def __init__(self): return super().__init__(404)
+
+async def not_found(request: Request, exc: NotFoundException):
+    return HTMLResponse(content='not found', status_code=exc.status_code)
+
+exception_handlers = { NotFoundException: not_found }
+
+htmxscr = Script(
+    src="https://unpkg.com/htmx.org@1.9.12", crossorigin="anonymous",
+    integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2")
+picocss = Link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/@picocss/pico@latest/css/pico.min.css")
+
+rt = Router(headtags=[htmxscr, picocss])
+
+def todo_body():
+    return Body(Main(H1('Todo list'), Ul(*TODO_LIST), Div(id='current-todo'), cls='container'))
+
+@rt("/", 'POST')
+async def add_item(data: TodoItem):
     TODO_LIST.append(data)
-    return TODO_LIST
 
-@put("/{item_title:str}")
-async def update_item(item_title: str, data: TodoItem) -> list[TodoItem]:
+@rt("/{item_title:str}", 'PUT')
+async def update_item(item_title: str, data: TodoItem):
     todo_item = get_todo_by_title(item_title)
     todo_item.title = data.title
     todo_item.done = data.done
-    return TODO_LIST
 
-@get("/", media_type=MediaType.HTML)
-async def get_list(done: bool|None = None) -> str:
-    todos = TODO_LIST if done is None else [
-        item for item in TODO_LIST if item.done == done]
-    tlist = Ul(Li(item.tag()) for item in todos)
-    elems = [H1('TODO list'), tlist, Div(id='current-todo')]
-    htmxscr = Script(src="https://unpkg.com/htmx.org@1.9.12",
-                     integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2",
-                     crossorigin="anonymous")
-    head = [Title('TODO list'), htmxscr]
-    res = Html(Head(*head), Body(*elems))
-    return to_xml(res)
+@rt("/todos/{todoid}", 'DELETE')
+async def del_todo(request):
+    todoid = request.path_params['todoid']
+    TODO_LIST.remove(next(o for o in TODO_LIST if o.id == todoid))
+    return Div(hx_swap_oob='true', id='todo-details')
 
-app = Litestar(
-    route_handlers=[
-        create_static_files_router(path="/static", directories=["static"]),
-        get_list, add_item, update_item, get_todo_by_id
-    ]
-)
+@rt("/todos/{todoid}")
+async def get_todo_by_id(request):
+    todoid = request.path_params['todoid']
+    try: todo = next(o for o in TODO_LIST if o.id == todoid)
+    except: raise NotFoundException() from None
+    btn = Button('delete', hx_delete=f'/todos/{todo.id}',
+                 hx_target=f"#todo-{todo.id}",
+                 hx_swap="outerHTML")
+    return Div(Div(todo.title), btn, id='todo-details')
+
+@rt("/favicon.ico")
+async def favicon(request): return FileResponse('favicon.ico', media_type='image/x-icon')
+
+@rt("/")
+async def get_todos(request):
+    return (Title('TODO list'), todo_body())
+
+app = Starlette(debug=True, routes=rt.routes, exception_handlers=exception_handlers)
