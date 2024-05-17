b5e8bfd781bfc8e19f1616a9960e0f35a58301d5
Jeremy Howard <github@jhoward.fastmail.fm>
2024-05-17 18:08:47 +1000

    port to fasthtml

--- app.py
+++ app.py
@@ -1,62 +1,12 @@
 import uvicorn
 from dataclasses import dataclass
-from functools import wraps
 
 from fastcore.utils import *
 from fastcore.xml import *
 
-from starlette.applications import Starlette
-from starlette.routing import Route, Mount, WebSocketRoute
-from starlette.responses import Response, HTMLResponse, RedirectResponse, FileResponse
-from starlette.requests import Request
-from starlette.staticfiles import StaticFiles
-from starlette.exceptions import HTTPException
+from fasthtml import FastHTML
 
-@dataclass
-class TodoItem:
-    id: int; title: str; done: bool
-    def __xt__(self):
-        before = "console.log('Making a request!')"
-        after = "console.log('Done making a request!')"
-        link = A(self.title, href='#',
-                hx_get=f'/todos/{self.id}', hx_target="#current-todo",
-                hx_on__after_request=after, hx_on__before_request=before)
-        return Li(link, id=f'todo-{self.id}')
-
-TODO_LIST: list[TodoItem] = [
-    TodoItem("0", title="Start writing TODO list", done=True),
-    TodoItem("1", title="???", done=False),
-    TodoItem("2", title="Profit", done=False),
-]
-
-def wrap_root(resp, headtags):
-    title = Title('Page')
-    if isinstance(resp, tuple): title,resp = resp
-    return Html(Head(title, *headtags), resp)
-
-class Router:
-    def __init__(self, headtags=None, routes=None):
-        self.rt = routes or []
-        self.rd = {}
-        self.headtags = headtags or []
-
-    def __call__(self, path, meth='GET', cls=HTMLResponse):
-        def _inner(f):
-            @wraps(f)
-            async def g(request):
-                resp = await f(request)
-                if not resp or isinstance(resp, str): resp = cls(resp)
-                if isinstance(resp, Response): return resp
-                if 'HX-Request' not in request.headers:
-                    resp = wrap_root(resp, self.headtags)
-                return cls(to_xml(resp))
-            self.rd[(path,meth)] = Route(path, g, methods=[meth])
-            return g
-        return _inner
-
-    @property
-    def routes(self): return self.rt + list(self.rd.values())
-
+"""
 class NotFoundException(HTTPException):
     def __init__(self): return super().__init__(404)
 
@@ -65,22 +15,35 @@ async def not_found(request: Request, exc: NotFoundException):
 
 exception_handlers = { NotFoundException: not_found }
 
+rt = Router(headtags=[htmxscr, picocss])
+
+def wrap_root(resp, headtags):
+    title = Title('Page')
+    if isinstance(resp, tuple): title,resp = resp
+    return Html(Head(title, *headtags), resp)
+"""
+
+@dataclass
+class TodoItem:
+    title: str; id: int = -1; done: bool = False
+    def __xt__(self):
+        link = A(self.title, href='#',
+                hx_get=f'/todos/{self.id}', hx_target="#current-todo")
+        return Li(link, id=f'todo-{self.id}')
+
+TODO_LIST: list[TodoItem] = [
+    TodoItem(id=0, title="Start writing todo list", done=True),
+    TodoItem(id=1, title="???", done=False),
+    TodoItem(id=2, title="Profit", done=False),
+]
+
 htmxscr = Script(
     src="https://unpkg.com/htmx.org@1.9.12", crossorigin="anonymous",
     integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2")
 picocss = Link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/@picocss/pico@latest/css/pico.min.css")
 
-rt = Router(headtags=[htmxscr, picocss])
-
-@rt("/", 'POST')
-async def add_item(request):
-    data = await request.form()
-    new_id = str(len(TODO_LIST) + 1)
-    new_item = TodoItem(id=new_id, title=data['title'], done=False)
-    TODO_LIST.append(new_item)
-    return (new_item, mk_input(hx_swap_oob='true'))
-
-@rt("/{item_id}", 'PUT')
+"""
+@app.put("/{item_id}")
 async def update_item(request):
     data = await request.json()
     item_id = request.path_params['item_id']
@@ -88,41 +51,44 @@ async def update_item(request):
     todo_item.title = data['title']
     todo_item.done = data['done']
     return RedirectResponse("/", status_code=303)
+"""
 
-@rt("/favicon.ico")
+# debug=True, routes=rt.routes, exception_handlers=exception_handlers
+app = FastHTML()
+
+@app.get("/favicon.ico")
 async def favicon(request): return FileResponse('favicon.ico', media_type='image/x-icon')
+
+@app.post("/")
+async def add_item(todo:TodoItem):
+    todo.id = len(TODO_LIST)+1
+    TODO_LIST.append(todo)
+    return todo, mk_input(hx_swap_oob='true')
 
 def find_todo(tid):
     try: return next(o for o in TODO_LIST if o.id==tid)
     except: raise NotFoundException() from None
 
-@rt("/todos/{tid}", 'DELETE')
-async def del_todo(request):
-    todo = find_todo(request.path_params['tid'])
-    TODO_LIST.remove(todo)
+@app.delete("/todos/{tid}")
+async def del_todo(tid:int):
+    TODO_LIST.remove(find_todo(tid))
     return Div(hx_swap_oob='true', id='todo-details')
 
-@rt("/todos/{tid}")
-async def get_todo_by_id(request):
-    todo = find_todo(request.path_params['tid'])
+@app.get("/todos/{tid}")
+async def get_todo_by_id(tid:int):
+    todo = find_todo(tid)
     btn = Button('delete', hx_delete=f'/todos/{todo.id}',
                  hx_target=f"#todo-{todo.id}", hx_swap="outerHTML")
     return Div(Div(todo.title), btn, id='todo-details')
 
 def mk_input(**kw): return Input(type="text", name="title", placeholder="New Todo", id="new-todo-title", **kw)
 
-@rt("/")
-async def get_todos(request):
-    form = Form(
-        mk_input(),
-        Button("Add Todo", type="submit"),
-        method="post",
-        hx_post="/",
-        hx_target="#todo-list",
-        hx_swap="beforeend",
-        id="new-todo-form",
+@app.get("/")
+async def get_todos(req):
+    form = Form(mk_input(), Button("Add Todo", type="submit"), method="post",
+                hx_post="/", hx_target="#todo-list", hx_swap="beforeend", id="new-todo-form",)
+    todo_ul = Ul(*TODO_LIST, id="todo-list")
+    return Html(
+        Head(Title('TODO list'), htmxscr, picocss),
+        Body(Main(H1('Todo list'), form, todo_ul, Div(id='current-todo'), cls='container'))
     )
-    elems = H1('Todo list'), form, Ul(*TODO_LIST, id="todo-list"), Div(id='current-todo')
-    return (Title('TODO list'), Body(Main(*elems, cls='container')))
-
-app = Starlette(debug=True, routes=rt.routes, exception_handlers=exception_handlers)
